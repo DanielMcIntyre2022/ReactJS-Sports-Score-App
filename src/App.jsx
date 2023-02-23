a75192155fbc68292c5e6220ee61ae11786524cd
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-23 18:53:18 -0500

    created components folder and created Header component inside imported Header component within main app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Sports App</h1>
+      <Header/>
     </div>
   );
-}
+};
 
 export default App;
