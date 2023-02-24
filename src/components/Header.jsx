59a94cedd96f2f811a8e4a6e67ac8cf27d96206b
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-23 19:02:42 -0500

    initial completion of header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
+import { BiBasketball }  from 'react-icons/bi';
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+        <div className="header-content-left flex items-center p-5 bg-orange-200">
+            <BiBasketball className='h-10 w-10'/>
+            <h1 className='ml-5 underline'>Basketball Scores</h1>
+        </div>
+    </div>
   )
 }
 
