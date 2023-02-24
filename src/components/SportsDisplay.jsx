a4a59ac651b74ace3b6fe2ec5fc44606ead61f31
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-24 09:49:26 -0500

    changed fetch call to axios call and successfully retrived the data

--- src/components/SportsDisplay.jsx
+++ src/components/SportsDisplay.jsx
@@ -1,18 +1,20 @@
+import axios from "axios";
 
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key':  process.env.REACT_APP_API_KEY,
-		'X-RapidAPI-Host': process.env.REACT_API_API_HOST,
-	}
+  method: 'GET',
+  url: 'https://sportscore1.p.rapidapi.com/sports/1/players',
+  params: {page: '1'},
+  headers: {
+	'X-RapidAPI-Key':  process.env.REACT_APP_API_KEY,
+	'X-RapidAPI-Host': process.env.REACT_API_API_HOST,
+}
 };
 
-console.log(process.env.REACT_APP_API_KEY)
-
-fetch('https://sportscore1.p.rapidapi.com/sports/1/players?page=1', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
 
 function SportsDisplay() {
   return (
